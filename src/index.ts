370b2d91d2ae48f92b26f9300a8c189ef041ce66
max_tricky_lake <mdovhopo@student.unit.ua>
2019-12-20 19:06:48 +0200

    add index file to provide better import api

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import Query from './Query';
+
+export default Query;
+
+// Tools
+export { default as QueryBuilder }     from './QueryBuilder';
+export { default as RqlParser }        from './RqlParser';
+export { default as QueryStringifier } from './QueryStringifier';
+
+// Nodes
+
+// aggregate
+
+export { default as AggregateSelect }       from './nodes/aggregateNodes/AggregateSelect';
+export { default as AggregateFunctionNode } from './nodes/aggregateNodes/AggregateFunctionNode';
+
+// array
+
+export { default as In }  from './nodes/arrayNodes/In';
+export { default as Out } from './nodes/arrayNodes/Out';
+
+// binary
+
+export { default as Eqf } from './nodes/binaryNodes/Eqf';
+export { default as Eqn } from './nodes/binaryNodes/Eqn';
+export { default as Eqt } from './nodes/binaryNodes/Eqt';
+export { default as Ie }  from './nodes/binaryNodes/Ie';
+
+// logical
+
+export { default as And } from './nodes/logicalNodes/And';
+export { default as Not } from './nodes/logicalNodes/Not';
+export { default as Or }  from './nodes/logicalNodes/Or';
+
+// scalar
+
+export { default as Alike }     from './nodes/scalarNodes/Alike';
+export { default as AlikeGlob } from './nodes/scalarNodes/AlikeGlob';
+export { default as Contains }  from './nodes/scalarNodes/Contains';
+export { default as Eq }        from './nodes/scalarNodes/Eq';
+export { default as Ge }        from './nodes/scalarNodes/Ge';
+export { default as Gt }        from './nodes/scalarNodes/Gt';
+export { default as Le }        from './nodes/scalarNodes/Le';
+export { default as Lt }        from './nodes/scalarNodes/Lt';
+export { default as Ne }        from './nodes/scalarNodes/Ne';
+export { default as Like }      from './nodes/scalarNodes/Like';
+export { default as LikeGlob }  from './nodes/scalarNodes/LikeGlob';
+
+// Other
+
+export { default as Sort }    from './nodes/Sort';
+export { default as Select }  from './nodes/Select';
+export { default as Limit }   from './nodes/Limit';
+export { default as GroupBy } from './nodes/GroupBy';

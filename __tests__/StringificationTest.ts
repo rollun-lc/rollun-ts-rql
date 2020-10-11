ea0735216ced35ce64615f251e0dc143d8fa980f
max_tricky_lake <mdovhopo@student.unit.ua>
2020-10-11 12:24:14 +0300

    fix type annotations

--- __tests__/StringificationTest.ts
+++ __tests__/StringificationTest.ts
@@ -18,202 +18,209 @@ import Lt                    from '../dist/nodes/scalarNodes/Lt';
 import Ne                    from '../dist/nodes/scalarNodes/Ne';
 import GroupBy               from '../dist/nodes/GroupBy';
 import AggregateFunctionNode from '../dist/nodes/aggregateNodes/AggregateFunctionNode';
-import { Eqf, Eqn, Eqt, Ie } from "../dist";
+import { Eqf, Eqn, Eqt, Ie } from '../dist';
 
 const dataProvider = [
-	{
-		queryObject: new Query({
-			select: new Select([
-				'id', 'name', 'data', 'age'
-			]),
-			sort: new Sort({age: -1}),
-			limit: new Limit(10, 0),
-			query: new And([
-				new Or([
-					new Eq('name', 'Bob'),
-					new Eq('name', 'Jeff')]),
-				new And([
-					new Le('age', 23),
-					new Ge('age', 55)
-				])
-			])
-		}),
-		expectedString: 'select(id,name,data,age)&sort(-age)&limit(10,0)&and(or(eq(name,string:Bob),eq(name,string:Jeff)),and(le(age,23),ge(age,55)))',
-		message: 'first query'
-	},
-	{
-		queryObject: new Query({
-			select: new Select(['id', 'name', 'age']),
-			query: new And([
-				new And([
-					new In('name', ['Alfred', 'Ivan', 'Jared']),
-					new Out('id', [12335, 16725])
-				]),
-				new Or([
-					new Like('name', 'er'),
-					new Alike('name', 'aZa')
-				])
-			]),
-			limit: new Limit(13, 100)
-		}),
-		expectedString: 'select(id,name,age)&limit(13,100)&and(and(in(name,(Alfred,Ivan,Jared)),out(id,(12335,16725))),or(like(name,string:er),alike(name,string:aZa)))',
-		message: 'second query'
-	},
-	{
-		queryObject: new Query({
-			query: new Not([
-				new Or([
-					new Gt('age', 33),
-					new Lt('age', 100),
-					new Ne('id', 1823457234985)
-				])
-			])
-		}),
-		expectedString: 'not(or(gt(age,33),lt(age,100),ne(id,1823457234985)))',
-		message: 'third query'
-	},
-	{
-		queryObject: new Query({
-			limit: new Limit(20, 20)
-		}),
-		expectedString: 'limit(20,20)',
-		message: 'fourth query'
-	},
+  {
+    queryObject:    new Query({
+      select: new Select([
+        'id', 'name', 'data', 'age'
+      ]),
+      sort:   new Sort({ age: -1 }),
+      limit:  new Limit(10, 0),
+      query:  new And([
+        new Or([
+          new Eq('name', 'Bob'),
+          new Eq('name', 'Jeff') ]),
+        new And([
+          new Le('age', 23),
+          new Ge('age', 55)
+        ])
+      ])
+    }),
+    expectedString: 'select(id,name,data,age)&sort(-age)&limit(10,0)&and(or(eq(name,string:Bob),eq(name,string:Jeff)),and(le(age,23),ge(age,55)))',
+    message:        'first query'
+  },
+  {
+    queryObject:    new Query({
+      select: new Select([ 'id', 'name', 'age' ]),
+      query:  new And([
+        new And([
+          new In('name', [ 'Alfred', 'Ivan', 'Jared' ]),
+          new Out('id', [ 12335, 16725 ])
+        ]),
+        new Or([
+          new Like('name', 'er'),
+          new Alike('name', 'aZa')
+        ])
+      ]),
+      limit:  new Limit(13, 100)
+    }),
+    expectedString: 'select(id,name,age)&limit(13,100)&and(and(in(name,(string:Alfred,string:Ivan,string:Jared)),out(id,(12335,16725))),or(like(name,string:er),alike(name,string:aZa)))',
+    message:        'second query'
+  },
+  {
+    queryObject:    new Query({
+      query: new Not([
+        new Or([
+          new Gt('age', 33),
+          new Lt('age', 100),
+          new Ne('id', 1823457234985)
+        ])
+      ])
+    }),
+    expectedString: 'not(or(gt(age,33),lt(age,100),ne(id,1823457234985)))',
+    message:        'third query'
+  },
+  {
+    queryObject:    new Query({
+      limit: new Limit(20, 20)
+    }),
+    expectedString: 'limit(20,20)',
+    message:        'fourth query'
+  },
 
-	{
-		queryObject: new Query({
-			query: new And([
-				new Ne('name', null),
-				new Ne('age', 'null()'),
-			])
-		}),
-		expectedString: 'and(ne(name,null()),ne(age,null()))',
-		message: 'with null()/ null'
-	},
-	{
-		queryObject: new Query({
-			select: new Select(['id', 'name', 'age']),
-			query: new Eq('age', 45)
-		}),
-		expectedString: 'select(id,name,age)&eq(age,45)',
-		message: 'query 5'
-	},
-	{
-		queryObject: new Query({
-			select: new Select(['id', 'name', 'age']),
-			query: new And([
-				new Or([new Le('age', 45), new Ge('age', 45)]),
-				new Or([new Le('age', 45), new Ge('age', 45)])
-			])
-		}),
-		expectedString: 'select(id,name,age)&and(or(le(age,45),ge(age,45)),or(le(age,45),ge(age,45)))',
-		message: 'query 6'
-	},
-	{
-		queryObject: new Query({
-			select: new Select(['id', 'name', 'age']),
-			sort: new Sort({id: 1, age: 1})
-		}),
-		expectedString: 'select(id,name,age)&sort(+id,+age)',
-		message: 'query with multiple sort nodes'
-	},
-	{
-		queryObject: new Query({
-			select: new Select(['id', 'name', 'age']),
-			query: new Or([
-				new Like('name', '('),
-				new Like('name', ')'),
-				new Like('name', '-'),
-				new Like('name', '_'),
-				new Like('name', '.'),
-				new Like('name', '~'),
-				new Like('name', '*'),
-				new Like('name', '\''),
-				new Like('name', '!')
-			])
-		}),
-		expectedString: 'select(id,name,age)&or(like(name,string:%28),like(name,string:%29),like(name,string:%2D),like(name,string:%5F),like(name,string:%2E),like(name,string:%7E),like(name,string:%2A),like(name,string:%27),like(name,string:%21))',
-		message: 'query with special characters'
-	},
-	{
-		queryObject: new Query({
-			select: new Select(['id', 'name', 'age']),
-			query: new Or([
-				new Like('name', '=('),
-				new Like('name', '=)'),
-				new Like('name', '*-*'),
-				new Like('name', 'T_T'),
-				new Like('name', '0.0'),
-				new Like('name', '~qw'),
-				new Like('name', 'o*o'),
-				new Like('name', 'ars\''),
-				new Like('name', 'run!')
-			])
-		}),
-		expectedString: 'select(id,name,age)&or(like(name,string:%3D%28),like(name,string:%3D%29),like(name,string:%2A%2D%2A),like(name,string:T%5FT),like(name,string:0%2E0),like(name,string:%7Eqw),like(name,string:o%2Ao),like(name,string:ars%27),like(name,string:run%21))',
-		message: 'query with words that contain special characters'
-	},
-	{
-		queryObject: new Query({
-			select: new Select([
-				new AggregateFunctionNode('count', 'age'),
-				new AggregateFunctionNode('min', 'age'),
-				new AggregateFunctionNode('max', 'age'),
-				new AggregateFunctionNode('avg', 'age')
-			])
-		}),
-		expectedString: 'select(count(age),min(age),max(age),avg(age))',
-		message: 'query with words that contain special characters'
-	},
-	{
-		queryObject: new Query({
-			select: new Select(['id', 'name', 'age']),
-			query: new Or([
-				new Like('name', '123'),
-				new Like('name', 123),
-				new Like('id', '1123532144'),
-				new Like('id', 1123532144),
-			])
-		}),
-		expectedString: 'select(id,name,age)&or(like(name,string:123),like(name,123),like(id,string:1123532144),like(id,1123532144))',
-		message: 'type annotation check query'
-	},
-	{
-		queryObject: new Query({
-			group: new GroupBy(['id', 'age'])
-		}),
-		expectedString: 'groupby(id,age)',
-		message: 'GroupBy Node test'
-	},
-	{
-		queryObject: new Query({
-			query: new Or([
-				new Eqn('field_1'),
-				new Eqt('field_2'),
-				new Eqf('field_3'),
-				new Ie('field_4')
-			])
-		}),
-		expectedString: 'or(eqn(field%5F1),eqt(field%5F2),eqf(field%5F3),ie(field%5F4))',
-		message: 'Binary nodes test'
-	},
-	{
-		queryObject: new Query({
-			query: new Eqn('field_1'),
-			limit: new Limit(20, 0)
-		}),
-		expectedString: 'limit(20,0)&eqn(field%5F1)',
-		message: 'Single node'
-	}
+  {
+    queryObject:    new Query({
+      query: new And([
+        new Ne('name', null),
+        new Ne('age', 'null()')
+      ])
+    }),
+    expectedString: 'and(ne(name,null()),ne(age,null()))',
+    message:        'with null()/ null'
+  },
+  {
+    queryObject:    new Query({
+      select: new Select([ 'id', 'name', 'age' ]),
+      query:  new Eq('age', 45)
+    }),
+    expectedString: 'select(id,name,age)&eq(age,45)',
+    message:        'query 5'
+  },
+  {
+    queryObject:    new Query({
+      select: new Select([ 'id', 'name', 'age' ]),
+      query:  new And([
+        new Or([ new Le('age', 45), new Ge('age', 45) ]),
+        new Or([ new Le('age', 45), new Ge('age', 45) ])
+      ])
+    }),
+    expectedString: 'select(id,name,age)&and(or(le(age,45),ge(age,45)),or(le(age,45),ge(age,45)))',
+    message:        'query 6'
+  },
+  {
+    queryObject:    new Query({
+      select: new Select([ 'id', 'name', 'age' ]),
+      sort:   new Sort({ id: 1, age: 1 })
+    }),
+    expectedString: 'select(id,name,age)&sort(+id,+age)',
+    message:        'query with multiple sort nodes'
+  },
+  {
+    queryObject:    new Query({
+      select: new Select([ 'id', 'name', 'age' ]),
+      query:  new Or([
+        new Like('name', '('),
+        new Like('name', ')'),
+        new Like('name', '-'),
+        new Like('name', '_'),
+        new Like('name', '.'),
+        new Like('name', '~'),
+        new Like('name', '*'),
+        new Like('name', '\''),
+        new Like('name', '!')
+      ])
+    }),
+    expectedString: 'select(id,name,age)&or(like(name,string:%28),like(name,string:%29),like(name,string:%2D),like(name,string:%5F),like(name,string:%2E),like(name,string:%7E),like(name,string:%2A),like(name,string:%27),like(name,string:%21))',
+    message:        'query with special characters'
+  },
+  {
+    queryObject:    new Query({
+      select: new Select([ 'id', 'name', 'age' ]),
+      query:  new Or([
+        new Like('name', '=('),
+        new Like('name', '=)'),
+        new Like('name', '*-*'),
+        new Like('name', 'T_T'),
+        new Like('name', '0.0'),
+        new Like('name', '~qw'),
+        new Like('name', 'o*o'),
+        new Like('name', 'ars\''),
+        new Like('name', 'run!')
+      ])
+    }),
+    expectedString: 'select(id,name,age)&or(like(name,string:%3D%28),like(name,string:%3D%29),like(name,string:%2A%2D%2A),like(name,string:T%5FT),like(name,string:0%2E0),like(name,string:%7Eqw),like(name,string:o%2Ao),like(name,string:ars%27),like(name,string:run%21))',
+    message:        'query with words that contain special characters'
+  },
+  {
+    queryObject:    new Query({
+      select: new Select([
+        new AggregateFunctionNode('count', 'age'),
+        new AggregateFunctionNode('min', 'age'),
+        new AggregateFunctionNode('max', 'age'),
+        new AggregateFunctionNode('avg', 'age')
+      ])
+    }),
+    expectedString: 'select(count(age),min(age),max(age),avg(age))',
+    message:        'query with words that contain special characters'
+  },
+  {
+    queryObject:    new Query({
+      select: new Select([ 'id', 'name', 'age' ]),
+      query:  new Or([
+        new Like('name', '123'),
+        new Like('name', 123),
+        new Like('id', '1123532144'),
+        new Like('id', 1123532144)
+      ])
+    }),
+    expectedString: 'select(id,name,age)&or(like(name,string:123),like(name,123),like(id,string:1123532144),like(id,1123532144))',
+    message:        'type annotation check query'
+  },
+  {
+    queryObject:    new Query({
+      group: new GroupBy([ 'id', 'age' ])
+    }),
+    expectedString: 'groupby(id,age)',
+    message:        'GroupBy Node test'
+  },
+  {
+    queryObject:    new Query({
+      query: new Or([
+        new Eqn('field_1'),
+        new Eqt('field_2'),
+        new Eqf('field_3'),
+        new Ie('field_4')
+      ])
+    }),
+    expectedString: 'or(eqn(field%5F1),eqt(field%5F2),eqf(field%5F3),ie(field%5F4))',
+    message:        'Binary nodes test'
+  },
+  {
+    queryObject:    new Query({
+      query: new Eqn('field_1'),
+      limit: new Limit(20, 0)
+    }),
+    expectedString: 'limit(20,0)&eqn(field%5F1)',
+    message:        'Single node'
+  },
+  {
+    queryObject:    new Query({
+      query: new Eq('united_order_id', '100920e507')
+    }),
+    expectedString: 'eq(united%5Forder%5Fid,string:100920e507)',
+    message:        'Numbers with `e` - 100920e507, must be type of string'
+  }
 ];
 
 describe('Stringification', () => {
-	const testsAmount = dataProvider.length;
-	dataProvider.forEach((entry, index) => {
-		const {queryObject, expectedString, message} = entry;
-		test(`Test ${index + 1} of ${testsAmount}, ${message}`, () => {
-			const actualString = QueryStringifier.stringify(queryObject);
-			expect(actualString).toEqual(expectedString);
-		});
-	});
+  const testsAmount = dataProvider.length;
+  dataProvider.forEach((entry, index) => {
+    const { queryObject, expectedString, message } = entry;
+    test(`Test ${ index + 1 } of ${ testsAmount }, ${ message }`, () => {
+      const actualString = QueryStringifier.stringify(queryObject);
+      expect(actualString).toEqual(expectedString);
+    });
+  });
 });
--- src/QueryStringifier.ts
+++ src/QueryStringifier.ts
@@ -12,140 +12,167 @@ import Glob                  from './parser/Glob';
 import AbstractBinaryNode    from './nodes/binaryNodes/AbstractBinaryNode';
 
 export default class QueryStringifier {
-	static stringify(query: Query): string {
-		const result = Object.entries(query).reduce(
-			(accumulator, item) => {
-				const [key, node] = item;
-				accumulator += this.parseTopLevelNode(key, node);
-				return accumulator + this.addUnion(accumulator);
-			},
-			'');
+  static stringify(query: Query): string {
+    const result = Object.entries(query).reduce(
+      (accumulator, item) => {
+        const [ key, node ] = item;
+        accumulator += this.parseTopLevelNode(key, node);
+        return accumulator + this.addUnion(accumulator);
+      },
+      '');
 
-		return (result.substr(-1) === '&')
-			? result.substring(0, result.length - 1)
-			: result;
-	}
+    return (result.substr(-1) === '&')
+      ? result.substring(0, result.length - 1)
+      : result;
+  }
 
-	protected static encodeRql(value: string): string {
-		let encodedValue = encodeURIComponent(value);
-		while (encodedValue.match(/[\(\)\-\_\.\~\!\*\'\!]/g)) {
-			encodedValue = encodedValue
-				.replace('(', '%28')
-				.replace(')', '%29')
-				.replace('-', '%2D')
-				.replace('_', '%5F')
-				.replace('.', '%2E')
-				.replace('~', '%7E')
-				.replace('*', '%2A')
-				.replace('\'', '%27')
-				.replace('!', '%21');
-		}
-		return encodedValue;
-	}
+  protected static encodeRql(value: string): string {
+    let encodedValue = encodeURIComponent(value);
+    while (encodedValue.match(/[\(\)\-\_\.\~\!\*\'\!]/g)) {
+      encodedValue = encodedValue
+        .replace('(', '%28')
+        .replace(')', '%29')
+        .replace('-', '%2D')
+        .replace('_', '%5F')
+        .replace('.', '%2E')
+        .replace('~', '%7E')
+        .replace('*', '%2A')
+        .replace('\'', '%27')
+        .replace('!', '%21');
+    }
+    return encodedValue;
+  }
 
-	protected static parseTopLevelNode(key: string, node: AbstractNode) {
-		let cleanKey = key;
-		if (cleanKey[0] === '_') { // remove underscore to enable protected and private methods usage
-			cleanKey = key.slice(1);
-		}
-		const methodName = `parse${cleanKey[0].toUpperCase()}${cleanKey.slice(1)}`;
-		const handler = this[methodName];
-		if (handler) {
-			return this[methodName](node);
-		} else {
-			throw new Error(`Node parser with name "${methodName}" is not a function!`);
-		}
-	}
+  protected static parseTopLevelNode(key: string, node: AbstractNode) {
+    let cleanKey = key;
+    if (cleanKey[0] === '_') { // remove underscore to enable protected and private methods usage
+      cleanKey = key.slice(1);
+    }
+    const methodName = `parse${ cleanKey[0].toUpperCase() }${ cleanKey.slice(1) }`;
+    const handler    = this[methodName];
+    if (handler) {
+      return this[methodName](node);
+    } else {
+      throw new Error(`Node parser with name "${ methodName }" is not a function!`);
+    }
+  }
 
-	protected static parseGroupNode(node?: GroupBy): string {
-		if (!node) {
-			return '';
-		}
-		const fieldsAmount = node.fields.length;
-		const nodeArguments = node.fields.map((field, index) => {
-			return this.encodeRql(field) + (index + 1 < fieldsAmount ? ',' : '');
-		});
-		return `groupby(${nodeArguments.join('')})`;
-	}
+  protected static parseGroupNode(node?: GroupBy): string {
+    if (!node) {
+      return '';
+    }
+    const fieldsAmount  = node.fields.length;
+    const nodeArguments = node.fields.map((field, index) => {
+      return this.encodeRql(field) + (index + 1 < fieldsAmount ? ',' : '');
+    });
+    return `groupby(${ nodeArguments.join('') })`;
+  }
 
-	protected static parseSelectNode(node?: Select): string {
-		if (node) {
-			const encodedFieldNames =
-				node.fields.map((fieldName) => {
-						if (fieldName instanceof AggregateFunctionNode) {
-							return `${this.encodeRql(fieldName.function)}(${this.encodeRql(fieldName.field.toString())})`;
-						} else {
-							return this.encodeRql(fieldName);
-						}
-					}
-				);
-			return `select(${encodedFieldNames.join(',')})`;
-		}
-		return '';
-	}
+  protected static parseSelectNode(node?: Select): string {
+    if (node) {
+      const encodedFieldNames =
+              node.fields.map((fieldName) => {
+                  if (fieldName instanceof AggregateFunctionNode) {
+                    return `${ QueryStringifier.encodeRql(fieldName.function) }(${ QueryStringifier.encodeRql(fieldName.field.toString()) })`;
+                  } else {
+                    return QueryStringifier.encodeRql(fieldName);
+                  }
+                }
+              );
+      return `select(${ encodedFieldNames.join(',') })`;
+    }
+    return '';
+  }
 
-	protected static parseSortNode(node?: Sort): string {
-		let result;
-		if (node) {
-			result = Object.entries(node.sortOptions).reduce((accumulator, currentItem) => {
-				const [field, direction] = currentItem;
-				const parsedDirection = (direction === 1) ? '+' : '-';
-				accumulator += `${parsedDirection}${this.encodeRql(field)},`;
-				return accumulator;
-			}, 'sort(');
-			if (result.endsWith(',')) {
-				result = result.substring(0, result.length - 1);
-			}
-			result += ')';
-		} else {
-			result = '';
-		}
-		return result;
-	}
+  protected static parseSortNode(node?: Sort): string {
+    let result;
+    if (node) {
+      result = Object.entries(node.sortOptions).reduce((accumulator, currentItem) => {
+        const [ field, direction ] = currentItem;
+        const parsedDirection      = (direction === 1) ? '+' : '-';
+        accumulator += `${ parsedDirection }${ this.encodeRql(field) },`;
+        return accumulator;
+      }, 'sort(');
+      if (result.endsWith(',')) {
+        result = result.substring(0, result.length - 1);
+      }
+      result += ')';
+    } else {
+      result = '';
+    }
+    return result;
+  }
 
-	protected static parseLimitNode(node?: Limit): string {
-		return node ? `limit(${node.limit},${node.offset})` : '';
-	}
+  protected static parseLimitNode(node?: Limit): string {
+    return node ? `limit(${ node.limit },${ node.offset })` : '';
+  }
 
-	protected static parseQueryNode(node?: AbstractNode): string {
-		let result = '';
-		switch (true) {
-			case (node instanceof AbstractLogicalNode):
-				const logicalNode = <AbstractLogicalNode> node;
-				const nodeName = logicalNode.name;
-				result += logicalNode.subNodes.reduce((accumulator, currentItem) => {
-					return accumulator + `${this.parseQueryNode(currentItem)},`;
-				}, result);
-				result = (result.substr(-1) === ',') ? result.substring(0, result.length - 1) : result;
-				result = `${nodeName}(${result})`;
-				break;
+  protected static parseQueryNode(node?: AbstractNode): string {
+    let result = '';
+    switch (true) {
+      case (node instanceof AbstractLogicalNode):
+        const logicalNode = <AbstractLogicalNode>node;
+        const nodeName    = logicalNode.name;
+        result += logicalNode.subNodes.reduce((accumulator, currentItem) => {
+          return accumulator + `${ this.parseQueryNode(currentItem) },`;
+        }, result);
+        result            = (result.substr(-1) === ',') ? result.substring(0, result.length - 1) : result;
+        result            = `${ nodeName }(${ result })`;
+        break;
 
-			case (node instanceof AbstractScalarNode):
-				const scalarNode = <AbstractScalarNode> node;
-				const nodeValue = scalarNode.value instanceof Glob ? scalarNode.value.toString() : scalarNode.value;
-				const type = (typeof nodeValue === 'string' && nodeValue !== 'null()' && nodeValue !== '' ? 'string:' : '');
-				const value = (nodeValue === '' || nodeValue === null || nodeValue === 'null()'
-					? 'null()'
-					: this.encodeRql(nodeValue));
-				result = `${scalarNode.name}(${this.encodeRql(scalarNode.field)},${type}${value})`;
-				break;
+      case (node instanceof AbstractScalarNode):
+        const scalarNode = <AbstractScalarNode>node;
+        const nodeValue  = scalarNode.value instanceof Glob ? scalarNode.value.toString() : scalarNode.value;
+        result           =
+          `${ QueryStringifier.withEncoding(scalarNode.name, { isField: true }) }(${
+            QueryStringifier.withEncoding(scalarNode.field, { isField: true }) },${
+            QueryStringifier.withType(QueryStringifier.withEncoding(nodeValue)) })`;
+        break;
 
-			case (node instanceof AbstractArrayNode):
-				const arrayNode = <AbstractArrayNode> node;
-				const encodedValues = arrayNode.values.map((value) => {
-					return this.encodeRql(value);
-				});
-				result = `${arrayNode.name}(${this.encodeRql(arrayNode.field)},(${encodedValues.join(',')}))`;
-				break;
-			case (node instanceof AbstractBinaryNode):
-				const binaryOperatorNode = <AbstractBinaryNode> node;
-				result = `${binaryOperatorNode.name}(${this.encodeRql(binaryOperatorNode.field)})`;
-				break;
-		}
-		return result;
-	}
+      case (node instanceof AbstractArrayNode):
+        const arrayNode     = <AbstractArrayNode>node;
+        const encodedValues = arrayNode.values.map(value =>
+          QueryStringifier.withType(QueryStringifier.withEncoding(value)));
 
-	protected static addUnion(queryString: string): string {
-		return (queryString !== '' && queryString.substr(-1) !== '&') ? '&' : '';
-	}
+        result = `${ QueryStringifier.withEncoding(arrayNode.name, { isField: true }) }(${
+          QueryStringifier.withEncoding(arrayNode.field, { isField: true }) },(${ encodedValues.join(',') }))`;
+        break;
+      case (node instanceof AbstractBinaryNode):
+        const binaryOperatorNode = <AbstractBinaryNode>node;
+        result                   = `${ binaryOperatorNode.name }(${ QueryStringifier.encodeRql(binaryOperatorNode.field) })`;
+        break;
+    }
+    return result;
+  }
+
+  /**
+   * Returns type `string:` if it is string, anf empty string, otherwise
+   * @param value
+   * @private
+   */
+
+  private static withType(value: number | string | null | undefined): string {
+    return `${ typeof value === 'string' && value !== 'null()' && value !== '' ? 'string:' : '' }${ value }`;
+  }
+
+  /**
+   * Encodes rql value and replaces empty strings and `null` with null()
+   * @param value
+   * @param isField
+   * @private
+   */
+
+  private static withEncoding(value: number | string | null | undefined, { isField = false } = {}): string | number {
+    if (isField) {
+      if (!value) throw new Error(`Invalid field value: [${ value }], expected number or string`);
+      return typeof value === 'number' ? value : QueryStringifier.encodeRql(value.toString());
+    }
+    return (value === '' || value === null || value === undefined || value === 'null()'
+      ? 'null()'
+      : (typeof value === 'number' ? value : QueryStringifier.encodeRql(value.toString())));
+  }
+
+  protected static addUnion(queryString: string): string {
+    return (queryString !== '' && queryString.substr(-1) !== '&') ? '&' : '';
+  }
 }
